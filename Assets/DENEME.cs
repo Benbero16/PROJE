fea6c550907153715293d6f5b91b01b5ae3326d5
Berat Yiğit Uğurlu <243005015@stu.mudanya.edu.tr>
2025-07-30 15:12:17 +0300

    Update DENEME.cs

--- Assets/DENEME.cs
+++ Assets/DENEME.cs
@@ -1,16 +1,27 @@
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class DENEME : MonoBehaviour
 {
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    private void Start()
     {
-        
+        /*  int damage = 10;
+          Debug.Log("verdiðiniz zarar : " +  damage); */
+
+        double ondalikliBir = 1 / 3d;
+        Debug.Log("Double Sonuç: " + ondalikliBir);
+
+        float ondalikliÝki = 1 / 3f;
+        Debug.Log("Float Sonuç: " + ondalikliÝki);
     }
 
     // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+  
+
     }
+    
 }
