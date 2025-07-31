9341066c47aa48b3bc310889fecbdf5a4359f2eb
Berat Yiğit Uğurlu <243005015@stu.mudanya.edu.tr>
2025-07-31 16:00:15 +0300

    temellere_devam

--- Assets/DENEME.cs
+++ Assets/DENEME.cs
@@ -1,16 +1,44 @@
 using Unity.VisualScripting;
 using UnityEngine;
-
+using System.Collections.Generic;
 public class DENEME : MonoBehaviour
 {
-   
-    [SerializeField] public float urunFiyat1;
-    [SerializeField] public float urunFiyat2;
+
+int topla(int sayi_1 , int sayi_2)
+    {
+        return sayi_1 + sayi_2;
+
+
+    }
+    int cikar(int sayi_1, int sayi_2)
+    {
+        return sayi_1 - sayi_2;
+    }
+
+    int carpma(int sayi_1, int sayi_2)
+    {
+        return sayi_1 * sayi_2;
+    }
+
+    float bol(int sayi_1, int sayi_2)
+    {
+        if (sayi_2 != 0)
+            return (float) sayi_1 / sayi_2;
+        else
+            return float.NaN; // veya hata mesajý/log ekleyebilirsin
+    }
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     private void Start()
     {
-        /*int damage = 10;
-        Debug.Log("verdiðiniz zarar : " +  damage); */
+        int sonuc_1 = topla(8, 4);
+        Debug.Log(sonuc_1);
+        int sonuc_2 = carpma(8, 4);
+        Debug.Log(sonuc_2);
+        int sonuc_3 = cikar(8, 4);
+        Debug.Log(sonuc_3);
+        float sonuc_4 = bol(8 , 4);
+Debug.Log(sonuc_4);
 
 
     }
@@ -32,48 +60,54 @@ public class DENEME : MonoBehaviour
           {
               Debug.Log("Bu sayý negatiftir");
           }
-        */
+        
+         int sinir = 20;
+              int ekUcretKg = 10;
+
+              if (bagajAgirligi <= sinir)
+              {
+                  Debug.Log("Herhangi bir ücret ödemeniz gerekmiyor.");
+              }
+              else
+              {
+                  int fazlaKg = bagajAgirligi - sinir;
+                  int toplamUcret = fazlaKg * ekUcretKg;
+                  Debug.Log("Fazla bagaj için " + toplamUcret + " TL ödemelisiniz.");
+              }  
+      switch (secim) {
+                  case "+":
+                      Debug.Log("Toplam: " + (sayi_1 + sayi_2));
+                      break;
+                  case "-":
+                      Debug.Log("Fark: " + (sayi_1 - sayi_2));
+                      break;
+                  case "*":
+                      Debug.Log("Çarpým: " + (sayi_1 * sayi_2));
+                      break;
+                  case "/":
+                      if (sayi_2 != 0)
+                          Debug.Log("Bölüm: " + (sayi_1 / sayi_2));
+                      else
+                          Debug.Log("Bölme iþlemi için ikinci sayý sýfýr olamaz!");
+                      break;
+                  default:
+                      Debug.Log("Geçersiz seçim! ");
+                      break;
 
 
 
-        /* int sinir = 20;
-        int ekUcretKg = 10;
-
-        if (bagajAgirligi <= sinir)
-        {
-            Debug.Log("Herhangi bir ücret ödemeniz gerekmiyor.");
-        }
-        else
-        {
-            int fazlaKg = bagajAgirligi - sinir;
-            int toplamUcret = fazlaKg * ekUcretKg;
-            Debug.Log("Fazla bagaj için " + toplamUcret + " TL ödemelisiniz.");
-        }  */
-        float toplamFiyat = urunFiyat1 + urunFiyat2;
-
-        if (toplamFiyat <= 200)
-        {
-            Debug.Log("Ödenecek miktar = " + toplamFiyat + " TL");
-        }
-        else
-        {
-            float indirimliFiyat = toplamFiyat * 0.75f; // %25 indirim
-            Debug.Log("Ödenecek miktar, indirimden sonra " + indirimliFiyat + " TL'dir.");
-        }
 
 
+
+              }
+
+              */
+
+        
+     
     }
-
-}
+    }
 //[SerializeField] public int b = 10;
 // Mantýksal Operatörler && (ve) , || (veya) ! (deðil)
-/*
-c) Bir hava yolu firmasý en fazla 20 kilogram bagaj hakký vermektedir. 20 kilogramdan sonraki her kilogram
-için 10 TL ek ücret almaktadýr. Buna göre bagajý 20 kg ya da daha az olan yolculara “Herhangi bir ücret
-ödemeniz gerekmiyor.”; 20 kg’den fazla olanlar için de ne kadar ek ücret ödeneceðini hesaplayarak “Fazla
-bagaj için ….. TL ödemelisiniz.” çýktýlarýný veren kodu yazýnýz.
-Not: Bu soruda kilogram hesabýnda sadece tam sayýlarý dikkate alýnýz. Örneðin 28,70 kilogram olan bagaj
-için sadece 8 kg için ek ücret ödenmesi yeterlidir.
-ç) Kullanýcýnýn girdiði iki ürünün toplam fiyatý 200 TL ve altýysa “Ödenecek miktar=…. TL”; 200 TL’yi geçerse
-%25 indirim yaparak “Ödenecek miktar, indirimden sonra ….. TL’dir.” çýktýlarýný veren kodu yazýnýz.  */
+
 
