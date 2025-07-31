3393705c3c36c5b355dabbfb677a3f6451bf66ab
Berat Yiğit Uğurlu <243005015@stu.mudanya.edu.tr>
2025-07-31 09:41:26 +0300

    denemelerden bir deneme

--- Assets/DENEME.cs
+++ Assets/DENEME.cs
@@ -3,25 +3,43 @@ using UnityEngine;
 
 public class DENEME : MonoBehaviour
 {
-
+    [SerializeField] public int sayi;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     private void Start()
     {
-        /*  int damage = 10;
-          Debug.Log("verdiðiniz zarar : " +  damage); */
+        /*int damage = 10;
+        Debug.Log("verdiðiniz zarar : " +  damage); */
 
-        double ondalikliBir = 1 / 3d;
-        Debug.Log("Double Sonuç: " + ondalikliBir);
 
-        float ondalikliÝki = 1 / 3f;
-        Debug.Log("Float Sonuç: " + ondalikliÝki);
     }
 
     // Update is called once per frame
     private void Update()
     {
-  
+      /*  if (sayi == 0)
+        {
+            Debug.Log("Bu sayý 0 a eþit");
+        }
+        else if (sayi > 0)
+        {
+
+            Debug.Log("Bu sayý pozitiftir");
+
+        }
+        else
+        {
+            Debug.Log("Bu sayý negatiftir");
+        }
+      */
+
+
+
+
+
 
     }
     
 }
+//[SerializeField] public int b = 10;
+// Mantýksal Operatörler && (ve) , || (veya) ! (deðil)
+
